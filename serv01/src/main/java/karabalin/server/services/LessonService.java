bae3a4dd5273dfc2292f542266b83ab85c7ac88a
Ruslan Karabalin <karabalin112@gmail.com>
2023-11-17 15:00:56 +0600

    [+] LessonService

--- serv01/src/main/java/karabalin/server/services/LessonService.java
+++ serv01/src/main/java/karabalin/server/services/LessonService.java
@@ -1,4 +1,90 @@
 package karabalin.server.services;
 
-public class LessonService {
+import karabalin.server.entities.LessonDTO;
+import karabalin.server.exceptions.RepositoryException;
+import karabalin.server.exceptions.ServiceException;
+import karabalin.server.repositories.interfaces.ILessonRepository;
+import karabalin.server.services.interfaces.ILessonService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class LessonService implements ILessonService {
+    private final ILessonRepository lessonRepository;
+
+    public LessonService(ILessonRepository lessonRepository) {
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Override
+    public long add(LessonDTO lesson) throws ServiceException {
+        try {
+            var result = lessonRepository.add(lesson);
+            if ( result == null ) {
+                throw new ServiceException("Teacher od Group not found");
+            }
+            return result;
+        } catch (RepositoryException e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void update(LessonDTO lesson) throws ServiceException {
+        try {
+            if (lessonRepository.update(lesson) == null) {
+                throw new ServiceException("Lesson with id =" + lesson.id() + " not found");
+            }
+        } catch (RepositoryException e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void delete(long id) throws ServiceException {
+        try {
+            lessonRepository.deleteById(id);
+        } catch (RepositoryException e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public LessonDTO get(long id) throws ServiceException {
+        try {
+            var result = lessonRepository.getById(id);
+            if (result == null) {
+                throw new ServiceException("Lesson with id = " + id + " not found");
+            }
+            return result;
+        } catch (RepositoryException e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public List<LessonDTO> getLessonsByGroup(LocalDate start, LocalDate end, long id) throws ServiceException {
+        try {
+            var result = lessonRepository.getLessonsByGroup(start, end, id);
+            if (result == null) {
+                throw new ServiceException("Group with id = " + id + " not found");
+            }
+            return result;
+        } catch (RepositoryException e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public List<LessonDTO> getLessonsByTeacher(LocalDate start, LocalDate end, long id) throws ServiceException {
+        try {
+            var result = lessonRepository.getLessonsByTeacher(start, end, id);
+            if (result == null) {
+                throw new ServiceException("Teacher with id = " + id + " not found");
+            }
+            return result;
+        } catch (RepositoryException e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
 }
--- serv01/src/main/java/karabalin/server/services/interfaces/ILessonService.java
+++ serv01/src/main/java/karabalin/server/services/interfaces/ILessonService.java
@@ -1,4 +1,16 @@
 package karabalin.server.services.interfaces;
 
+import karabalin.server.entities.LessonDTO;
+import karabalin.server.exceptions.ServiceException;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ILessonService {
+    long add(LessonDTO lesson) throws ServiceException;
+    void update(LessonDTO lesson) throws ServiceException;
+    void delete(long id) throws ServiceException;
+    LessonDTO get(long id) throws ServiceException;
+    List<LessonDTO> getLessonsByGroup(LocalDate start, LocalDate end, long id) throws ServiceException;
+    List<LessonDTO> getLessonsByTeacher(LocalDate start, LocalDate end, long id) throws ServiceException;
 }
