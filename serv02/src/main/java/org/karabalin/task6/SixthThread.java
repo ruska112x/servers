ff367526fc2247a451c953114a2164951bc30401
Ruslan Karabalin <karabalin112@gmail.com>
2024-03-12 14:04:27 +0600

    [$] pass 6

--- serv02/src/main/java/org/karabalin/task6/SixthThread.java
+++ serv02/src/main/java/org/karabalin/task6/SixthThread.java
@@ -14,25 +14,25 @@ public class SixthThread extends Thread {
     public void run() {
         if (add) {
             for (int i = 0; i < 10000; ++i) {
+                sixthTask.adding();
+                System.out.println("A" + i);
                 synchronized (sixthTask) {
-                    sixthTask.adding();
-                    System.out.println("A" + i);
                     sixthTask.notify();
                 }
             }
         } else {
             for (int i = 0; i < 10000; ++i) {
-                synchronized (sixthTask) {
-                    if (sixthTask.isEmpty()) {
+                if (sixthTask.isEmpty()) {
+                    synchronized (sixthTask) {
                         try {
                             sixthTask.wait();
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                     }
-                    sixthTask.subtracting();
-                    System.out.println("D" + i);
                 }
+                sixthTask.subtracting();
+                System.out.println("D" + i);
             }
         }
     }
