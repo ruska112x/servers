cd04a9f0c0c6404f28cf36cd29d0ef4a76bf7a7f
Ruslan Karabalin <karabalin112@gmail.com>
2024-03-21 14:45:10 +0600

    feat: start task 3

---
+++ serv3/src/main/java/org/karabalin/serv3/Serv3Application.java
@@ -0,0 +1,13 @@
+package org.karabalin.serv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Serv3Application.class, args);
+	}
+
+}
---
+++ serv3/src/test/java/org/karabalin/serv3/Serv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.karabalin.serv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
